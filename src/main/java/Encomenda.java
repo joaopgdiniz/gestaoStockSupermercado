f701d7c7b8fd50f2b3fe7ab71b71306d9365f520
João Diniz <joaopgdiniz03@gmail.com>
2024-04-24 12:28:34 +0100

    Class Encomenda

--- src/main/java/Encomenda.java
+++ src/main/java/Encomenda.java
@@ -1,4 +1,32 @@
+import java.util.Date;
+
 public class Encomenda {
     private int codEncomenda;
+    private Produto produto;
+    private int quantidade;
+    private Date dataEncomenda;
+
+    public Encomenda(int codEncomenda, Produto produto, int quantidade, Date dataEncomenda) {
+        this.codEncomenda = codEncomenda;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.dataEncomenda = dataEncomenda;
+    }
+
+    public int getCodEncomenda() {
+        return codEncomenda;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public Date getDataEncomenda() {
+        return dataEncomenda;
+    }
 
 }
