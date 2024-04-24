d23968f344a39207778a74d0a84f11c62af75a29
José Rodrigues <2211003@my.ipleiria.pt>
2024-04-24 13:25:45 +0100

    interface gestorStock.form

---
+++ src/main/java/alterarEstado.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class alterarEstado {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JButton button1;
+}
---
+++ src/main/java/consultarProduto.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class consultarProduto {
+    private JButton consultarProdutosButton;
+    private JButton registarLoteButton;
+    private JButton registarProdutoButton;
+    private JTable table1;
+}
